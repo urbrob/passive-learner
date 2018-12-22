d53f054feb6943259ebf9a2c88d2d771c5bb40d5
MKubik <bopke2@gmail.com>
2018-12-22 12:14:45 +0100

    add deleting local file ability. Closes #8

--- APP/app/src/main/java/passive_learner/urbrob/github/com/app/FileManager.kt
+++ APP/app/src/main/java/passive_learner/urbrob/github/com/app/FileManager.kt
@@ -3,12 +3,15 @@ package passive_learner.urbrob.github.com.app
 import android.content.Context
 
 class FileManager(context: MainActivity) {
-    val ctx: MainActivity = context
+    private val ctx: MainActivity = context
 
-    fun saveFileToLocalResources(fileName: String, content: String) {
+    fun saveToLocalFile(fileName: String, content: String) {
         ctx.openFileOutput(fileName, Context.MODE_PRIVATE).use {
             it.write(content.toByteArray())
         }
+    }
+    fun deleteLocalFile(fileName: String) {
+        ctx.deleteFile(fileName)
     }
 
 }
