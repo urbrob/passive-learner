d00ba77b1339032e80f8b26d4a0604e4393e66f4
Łukasz Zalewski <zalewski.ukas@gmail.com>
2019-01-03 15:36:15 +0100

    Models improvements

--- API/groups/models.py
+++ API/groups/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_creation = models.DateField(auto_now_add=True)
+    verified = models.BooleanField(default=False)
+
+
 class Question(models.Model):
-    content = models.TextField()
+    content = models.TextField(max_length=1000)
 
     def __str__(self):
     	return self.content
@@ -11,14 +17,14 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    true = models.BooleanField()
+    content = models.CharField(max_length=200)
+    true = models.BooleanField(default=False)
 
 
 class QuestionSet(models.Model):
     name = models.CharField(max_length=100)
     questions = models.ManyToManyField(Question)
-    public = models.BooleanField()
+    public = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -32,13 +38,14 @@ class Copy(models.Model):
 
 class Group(models.Model):
     name = models.CharField(max_length=40)
-    description = models.TextField()
+    description = models.TextField(500)
+    users = models.ManyToManyField(User, through='Membership')
 
     def __str__(self):
     	return self.name
 
 
-class Membership():
+class Membership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     MEMBER = 'member'
@@ -47,12 +54,12 @@ class Membership():
             (MEMBER, 'Member'),
             (MODERATOR, 'Moderator')
     )
-    role = models.CharField(max_length=10, choices=ROLES)
+    role = models.CharField(max_length=10, choices=ROLES, default=MEMBER)
 
 
 class Categories(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(300)
     question_sets = models.ManyToManyField(QuestionSet)
     group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
 
