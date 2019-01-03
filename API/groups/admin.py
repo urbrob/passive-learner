43a5dc6260b5fbb70aabf6eb6af7f8f05d05320f
Witach <witaszek98@wp.pl>
2019-01-03 21:11:01 +0100

    adminview for UserInfo added

--- API/groups/admin.py
+++ API/groups/admin.py
@@ -1,62 +1,97 @@
 from django.contrib import admin
-from .models import Category,SetOfQuestion,Group,Question,Answer,Membership,Copy
+from .models import Category, SetOfQuestion, Group, Question, Answer, Membership, Copy, UserInfo
 # Register your models here.
+
+
 class CategoryAdmin(admin.ModelAdmin):
     fieldsests = (
-                    ('Information',{'fields':('Name','Description')}),
-                    ('Sets',{'fields':('SetsOfQuestions')}),
-                    )
-    list_display = ('Name','group_name','count_of_sets')
-    search_fields = ('Name','group_name','count_of_sets')
-    def group_name(self,obj):
-        return obj.Group
-    def count_of_sets(self,obj):
+        ('Information', {'fields': ('Name', 'Description')}),
+        ('Sets', {'fields': ('SetsOfQuestions')}),
+    )
+    list_display = ('Name', 'group_name', 'count_of_sets')
+    search_fields = ('Name', 'group_name', 'count_of_sets')
+
+    def group_name(self, obj):
+        return obj.group
+
+    def count_of_sets(self, obj):
         return obj.SetsOfQuestions.all().count()
-admin.site.register(Category,CategoryAdmin)
+
+
+admin.site.register(Category, CategoryAdmin)
+
 
 class CategoryInline(admin.StackedInline):
     model = Category
+
+
 class GroupAdmin(admin.ModelAdmin):
     fieldsests = (
-                    ('Information',{'fields':('Name','Description')}),
-                    )
-    inlines = [CategoryInline,]
-    list_display = ('Name','count_of_categories')
-    search_fields = ('Name','count_of_categories')
-    def count_of_categories(self,obj):
-        return Category.objects.get(Group=obj).count()
-admin.site.register(Group,GroupAdmin)
+        ('Information', {'fields': ('name', 'description')}),
+    )
+    inlines = [CategoryInline, ]
+    list_display = ('name', 'count_of_categories')
+    search_fields = ('name', 'count_of_categories')
+
+    def count_of_categories(self, obj):
+        return Category.objects.get(group=obj).count()
+
+
+admin.site.register(Group, GroupAdmin)
+
 
 class MembershipAdmin(admin.ModelAdmin):
-    fields = ('Group','User','Role')
-    list_display = ('Group','User','Role')
-    search_fields = ('Group','User','Role')
-    list_filter = ('Role')
-admin.site.register(Membership,MembershipAdmin)
+    fields = ('group', 'user', 'role')
+    list_display = ('group', 'user', 'role')
+    search_fields = ('group', 'user', 'role')
+    list_filter = ('role')
 
-class SetOfQuestionsAdmin(admin.ModelAdmin):
+
+admin.site.register(Membership, MembershipAdmin)
+
+
+class QuestionSetAdmin(admin.ModelAdmin):
     fieldsests = (
-                    ('Information',{'fields':('Name','Author','Public')}),
-                    ('Questions',{'fields':('Question')}),
-                    )
-    list_display = ('Name','Author','Public')
-    list_editable = ('Public')
-    list_filter = ('Public')
-    search_fields = ('Name','Author')
-admin.site.register(SetOfQuestions,SetOfQuestionsAdmin)
+        ('Information', {'fields': ('name', 'author', 'public')}),
+        ('Questions', {'fields': ('question')}),
+    )
+    list_display = ('name', 'author', 'public')
+    list_editable = ('public', )
+    list_filter = ('public', )
+    search_fields = ('name', 'author')
+
+
+admin.site.register(SetOfQuestions, SetOfQuestionsAdmin)
+
 
 class AnswerAdmin(admin.ModelAdmin):
-    fields = ('Content','True','Question')
-    list_display = ('Content','True')
-    list_filter = ('True')
-admin.site.register(Answer,AnswerAdmin)
+    fields = ('content', 'true', 'question')
+    list_display = ('content', 'true')
+    list_filter = ('true', )
+
+admin.site.register(Answer, AnswerAdmin)
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ('Content')
-    list_display = ('Content','count_of_answer')
+    fields = ('content', )
+    list_display = ('content', 'count_of_answer')
     list_filter = ('count_of_answer')
-    def count_of_answer(self,obj):
-        return Answer.objects.get(Question=obj).count()
-admin.site.register(Question,QuestionAdmin)
+
+    def count_of_answer(self, obj):
+        return Answer.objects.get(question=obj).count()
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class UserInfoAdmin(models.ModelAdmin):
+    fields = ('user', 'date_of_creation', 'verified')
+    list_filter = ('verified', )
+    list_display = ('user', 'date_of_creation', 'verified')
+    search_fields = ('user', 'date_of_creation')
+
+
+admin.site.register(UserInfo,UserInfoAdmin)
+
 
 admin.site.register(Copy)
